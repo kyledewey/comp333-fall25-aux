f539163f15e1e040f214a78b48180cdd58fa748e
Kyle Dewey <kyle.dewey@csun.edu>
2025-09-03 12:23:07 -0700

    three separate behaviors for writing

--- section_1/java/Writer.java
+++ section_1/java/Writer.java
@@ -1,14 +1,33 @@
 public class Writer {
     private BufferedWriter writer = null;
     private Socket socket = null;
-    private String destination = null;
-    private NetworkLocation location = null;
+    private final String destination = null;
+    private final NetworkLocation location = null;
+
+    public Writer(final String destination) {
+        // only writing to files
+        this.destination = destination;
+    }
+    public Writer(final NetworkLocation location) {
+        // only writing to network
+        this.location = location;
+    }
 
     public Writer(final String destination,
                   final NetworkLocation location) {
+        // writing to BOTH file and network
         this.destination = destination;
         this.location = location;
     }
+    
+    // public Writer(final String destination,
+    //               final NetworkLocation location) {
+    //     assert ((destination == null && location == null) ||
+    //             (destination != null && location == null) ||
+    //             (destination == null && location != null));
+    //     this.destination = destination;
+    //     this.location = location;
+    // }
 
     // problem: we need to maintain some state for open files (writer),
     // need to track if you've opened the file already

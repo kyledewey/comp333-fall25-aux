cf19c2e6a61c3d93fc5760212bc624dd68a81983
Kyle Dewey <kyle.dewey@csun.edu>
2025-09-29 15:54:00 -0700

    .

---
+++ section_2/javascript/Add.java
@@ -0,0 +1,18 @@
+public class Add {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static double add(double x, double y) {
+        return x + y;
+    }
+    public static String add(String x, String y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(3, 4));
+        System.out.println(add(2.2, 3.3));
+        System.out.println(add("foo", "bar"));
+    }
+}
+
---
+++ section_2/javascript/intro.js
@@ -0,0 +1,25 @@
+// public static int add(int x, int y) {
+//   return x + y;
+// }
+//
+// JavaScript: dynamically-typed (variables DON'T have
+//             fixed types)
+// Java: statically-typed (variables have fixed types)
+function add(x, y) {
+    return x + y;
+}
+
+// public static (String|int) doSomething(int x) {
+//   if (x < 5) {
+//     return "foo";
+//   } else {
+//     return 42;
+//   }
+// }
+function doSomething(x) {
+    if (x < 5) {
+        return "foo";
+    } else {
+        return 42;
+    }
+}

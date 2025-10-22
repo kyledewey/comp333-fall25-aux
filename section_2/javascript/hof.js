c26fd175f6ab86087d2bfda549b702059b33aa5a
Kyle Dewey <kyle.dewey@csun.edu>
2025-10-22 11:23:26 -0700

    .

--- section_2/javascript/hof.js
+++ section_2/javascript/hof.js
@@ -1,10 +1,32 @@
-function printAllLessThanN(arr, n) {
+// arr: array of values
+// compare: a function
+//   -Takes a value
+//   -Returns a boolean
+function printSome(arr, compare) {
     for (let index = 0; index < arr.length; index++) {
         let element = arr[index];
-        if (element < n) {
+        if (compare(element)) {
             console.log(element);
         }
     }
+}
+
+function printAllLessThanN(arr, n) {
+    printSome(arr, (e) => e < n);
+
+    // printSome(arr,
+    //           function (e) {
+    //               return e < n;
+    //           });
+}
+
+function printAllGreaterThanN(arr, n) {
+    printSome(arr, (e) => e > n);
+
+    // let temp = function (e) {
+    //     return e > n;
+    // };
+    // printSome(arr, temp);
 }
 
 function printAllLessThan5(arr) {

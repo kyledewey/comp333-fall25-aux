e33a3e8a01060c01371ac05600b4ad86d0fb0cd6
Kyle Dewey <kyle.dewey@csun.edu>
2025-11-05 17:16:32 -0800

    .

--- section_2/javascript/objects.js
+++ section_2/javascript/objects.js
@@ -1,8 +1,16 @@
-function makeRectangle(w, h) {
-    return { 'width' : w, 'height' : h };
+// function makeRectangle(w, h) {
+//     return { 'width' : w, 'height' : h };
+// }
+
+function Rectangle(w, h) {
+    // this = {};
+    this.width = w;
+    this.height = h;
+    // return this;
 }
 
 // rectangle: object with width and height fields
 function getArea(rectangle) {
-    return rectangle.width * rectangle.height;
+    let temp = rectangle.width * rectangle.height;
+    return temp;
 }

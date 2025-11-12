0c773b383d243c1c943dfb1e7879aa28055c4dc0
Kyle Dewey <kyle.dewey@csun.edu>
2025-11-12 12:26:31 -0800

    explicit prototype

--- section_1/javascript/objects.js
+++ section_1/javascript/objects.js
@@ -2,13 +2,20 @@
 //     return { 'width' : w, 'height' : h };
 // }
 
+let rectanglePrototype = {
+    'getArea': function() {
+        return this.width * this.height;
+    }
+};
+
 function Rectangle(w, h) {
     // this = {};
     this.width = w;
     this.height = h;
-    this.getArea = function() {
-        return this.width * this.height;
-    };
+    this.__proto__ = rectanglePrototype;
+    // this.getArea = function() {
+    //     return this.width * this.height;
+    // };
     // return this;
 }
 

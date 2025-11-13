1dbd50e1612ef21f4d451d121cdaaf594c56ebe9
Kyle Dewey <kyle.dewey@csun.edu>
2025-11-12 16:44:18 -0800

    .

--- section_2/javascript/objects.js
+++ section_2/javascript/objects.js
@@ -2,13 +2,17 @@
 //     return { 'width' : w, 'height' : h };
 // }
 
+let rectanglePrototype = {
+    'getArea': function() {
+        return this.width * this.height;
+    }
+};
+
 function Rectangle(w, h) {
     // this = {};
     this.width = w;
     this.height = h;
-    this.getArea = function() {
-        return this.width * this.height;
-    };
+    this.__proto__ = rectanglePrototype;
     // return this;
 }
 

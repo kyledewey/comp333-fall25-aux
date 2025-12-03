a7265d2350f06cc2b375c2576b3ace06c767eb9b
Kyle Dewey <kyle.dewey@csun.edu>
2025-12-03 12:07:12 -0800

    .

--- section_1/javascript/add_this.js
+++ section_1/javascript/add_this.js
@@ -7,3 +7,21 @@ console.log(withOne.add(1));
 console.log(withFive.add(2));
 
 AddThis.prototype.sub = function (y) { return y - this.x; };
+
+let temp = new AddThis();
+temp.mul = function (y) {
+    return y * this.x;
+};
+AddThis.prototype = temp;
+
+// AddThis.prototype = {
+//     'mul': function (y) {
+//         return y * this.x;
+//     },
+//     '__proto__': AddThis.prototype
+// };
+
+let withSeven = new AddThis(7);
+console.log(withSeven.mul(2)); // prints 14
+console.log(withOne.mul); // prints undefined
+console.log(withFive.mul); // prints undefined
--- section_2/javascript/objects_handout.js
+++ section_2/javascript/objects_handout.js
@@ -5,18 +5,18 @@ function Sub2() {}
 Base.prototype.method = function() {
     console.log("base");
 };
-Sub2.__proto__ = Base.prototype;
+Sub1.prototype = new Base();
+Sub2.prototype = new Base();
 Sub1.prototype.method = function() {
     console.log("sub1");
 };
-Sub1.__proto__ = Base.prototype;
 
 let base = new Base();
 let sub1 = new Sub1();
 let sub2 = new Sub2();
-// base.method(); // base
-// sub1.method(); // sub1
-// sub2.method(); // base
-// console.log(base instanceof Base); // true
-// console.log(sub1 instanceof Base); // true
-// console.log(sub2 instanceof Base); // true
+base.method(); // base
+sub1.method(); // sub1
+sub2.method(); // base
+console.log(base instanceof Base); // true
+console.log(sub1 instanceof Base); // true
+console.log(sub2 instanceof Base); // true
